984d1e8dd64307a95e13a1107c32545d5f372989
vrenc <vrencvd@gmail.com>
2015-11-18 23:35:10 +0100

    recursive login menu

--- app.rb
+++ app.rb
@@ -1,28 +1,39 @@
-require 'rubygems'
-require 'bundler/setup'
+require "rubygems"
+require "bundler/setup"
+require "multi_json"
 require "hyperresource"
-require 'highline/import'
+require "highline/import"
 require "./lib/auth.rb"
 require "./lib/lists.rb"
 require "./lib/todos.rb"
+require "./lib/tags.rb"
 
-api = HyperResource.new(root: 'http://localhost:3306',
-                    headers: {'Accept' => 'application/hal+json'})
+api = HyperResource.new(root: "http://localhost:3306",
+                    headers: {"Accept" => "application/hal+json"})
 root = api.get
 cookie = CGI::Cookie::parse root.get_response.headers["set-cookie"]
 session_cookie = /^[^;]+/.match(cookie["rack.session"].value.to_s)
 
-root.headers = {'Accept' => 'application/hal+json', 'Cookie' => "#{session_cookie}"}
+root.headers = {"Accept" => "application/hal+json", "Cookie" => "#{session_cookie}"}
 
-user = nil
 
-choose do |menu|
-    say "===================="
-    
-    menu.prompt = "Login or register  "
-    menu.choice(:login) { user = login root }
-    menu.choice(:register) { user = register root }
+def loginMenu(root)
+    user = nil
+    choose do |menu|
+        say "===================="
+        say "Login or register"
+        say "===================="
+        menu.choice(:login) { user = login root }
+        menu.choice(:register) { user = register root }
+    end
+
+    if user == nil then
+        user = loginMenu(root)
+    end
+
+    user
 end
 
+user = loginMenu(root)
 
 lists user
