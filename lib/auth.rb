f164c5cd7c427506e3a33f5cbcf8f760f1fc8616
vrenc <vrencvd@gmail.com>
2015-11-18 23:34:20 +0100

    auth error reporting

--- lib/auth.rb
+++ lib/auth.rb
@@ -2,12 +2,26 @@ def login(root)
     username = ask("Username:  ")
     password = ask("Password:  ") { |q| q.echo = "x" }
 
-    user = root.login.post username: username, password: password
+    begin
+        user = root.login.post username: username, password: password
+    rescue HyperResource::ClientError => e
+        say e.body["message"]
+        return nil
+    end
+
+    user
 end
 
 def register(root)
     username = ask("Username:  ")
     password = ask("Password:  ") { |q| q.echo = "x" }
 
-    root.register.post username: username, password: password
+    begin
+        user = root.register.post username: username, password: password
+    rescue HyperResource::ClientError => e
+        say e.body["message"]
+        return nil
+    end
+
+    user
 end
